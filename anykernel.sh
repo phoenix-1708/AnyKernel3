d3cb56419a3be283bd7f0862ff31293536056339
phoenix <62538135+phoenix-1708@users.noreply.github.com>
2021-08-08 11:25:54 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Delta Kernel by CloudedQuartz
+kernel.string=PhoenixKernel by Harikumar aka Phoenix1708
 do.devicecheck=1
 do.modules=0
 do.systemless=1
